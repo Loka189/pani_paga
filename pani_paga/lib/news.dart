be825b0d3eb026bfd95ff73bc872b72fb88f4aae
sonu <lokanath189@gmail.com>
2023-06-06 21:07:49 +0530

    initial commit

---
+++ pani_paga/lib/news.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NewsAPI extends StatefulWidget {
+  const NewsAPI({Key? key}) : super(key: key);
+
+  @override
+  State<NewsAPI> createState() => _NewsAPIState();
+}
+
+class _NewsAPIState extends State<NewsAPI> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
